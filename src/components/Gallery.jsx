12fbe708fa892867652d3f03b79077b96bef296d
An7orAhmed <antor.mee@gmail.com>
2023-10-31 21:21:25 +0600

    drop drag ok

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,20 +2,30 @@ import ImageBox from "./ImageBox";
 import AddImage from "./AddImage";
 import { useContext } from "react";
 import { ImagesContext } from "../pages/Home";
-
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 function Gallery() {
-  const { images } = useContext(ImagesContext);
+  const { images, setImages } = useContext(ImagesContext);
+
+  const moveImage = (fromIndex, toIndex) => {
+    const updatedImages = [...images];
+    const [movedImage] = updatedImages.splice(fromIndex, 1);
+    updatedImages.splice(toIndex, 0, movedImage);
+    setImages(updatedImages);
+  };
 
   return (
     <div>
       <div className="container mx-auto p-2 md:p-10 bg-white rounded-xl">
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-          {
-            images?.map((image, i) => <ImageBox key={i} image={image} isFeatured={i == 0}></ImageBox>)
-          }
-          <AddImage></AddImage>
-        </div>
+        <DndProvider backend={HTML5Backend}>
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+            {
+              images?.map((image, i) => <ImageBox key={i} image={image} index={i} moveImage={moveImage} isFeatured={i == 0}></ImageBox>)
+            }
+            <AddImage></AddImage>
+          </div>
+        </DndProvider>
       </div>
     </div>
   );
--- src/components/ImageBox.jsx
+++ src/components/ImageBox.jsx
@@ -1,28 +1,44 @@
 import PropTypes from 'prop-types';
 import { useContext, useEffect, useState } from 'react';
 import { ImagesContext } from '../pages/Home';
+import { useDrag, useDrop } from 'react-dnd';
 
 ImageBox.propTypes = {
   image: PropTypes.string,
-  isFeatured: PropTypes.bool
+  index: PropTypes.number,
+  isFeatured: PropTypes.bool,
+  moveImage: PropTypes.func,
 };
 
-function ImageBox({ image, isFeatured }) {
+function ImageBox({ image, index, isFeatured, moveImage }) {
   const { setectedImg, setSelectedImg } = useContext(ImagesContext);
   const [hover, setHover] = useState(false);
   const [selected, setSelected] = useState(false);
 
+  const [, drag] = useDrag({
+    type: 'image',
+    item: { index },
+  });
+
+  const [{ isActive }, drop] = useDrop({
+    accept: 'image',
+    collect: (monitor) => ({
+      isActive: monitor.canDrop() && monitor.isOver(),
+    }),
+    hover: (draggedItem) => {
+      if (draggedItem.index !== index) {
+        moveImage(draggedItem.index, index);
+        draggedItem.index = index;
+      }
+    },
+  });
+
   useEffect(() => {
-    if(setectedImg.length === 0) setSelected(false);
-  },[setectedImg]);
+    if (setectedImg.length === 0) setSelected(false);
+  }, [setectedImg]);
 
-  const onHover = () => {
-    setHover(true);
-  }
-
-  const onLeave = () => {
-    setHover(false);
-  }
+  const onHover = () => setHover(true);
+  const onLeave = () => setHover(false);
 
   const onSelect = (e) => {
     const state = e.target.checked;
@@ -33,20 +49,21 @@ function ImageBox({ image, isFeatured }) {
 
   const divClass = `
     ${selected ? "bg-white" : "hover:bg-black"}
-    relative border-2 rounded-md 
-    ${(isFeatured && "col-span-2 row-span-2")}
+    relative border-2 rounded-md
+    ${(isFeatured ? "col-span-2 row-span-2 h-full" : "h-fit")}
   `;
 
   const imgClass = `
     ${selected && "scale-75"}
     rounded-md hover:opacity-50 hover:bg-blend-darken transition-all duration-500 object-cover w-full
     ${(isFeatured ? "h-full" : "h-72")}
+    ${isActive && "opacity-30"}
   `;
 
   const checkClass = "absolute top-3 left-3 checkbox checkbox-accent checkbox-lg rounded-full";
 
   return (
-    <div onMouseEnter={onHover} onMouseLeave={onLeave} className={divClass}>
+    <div ref={(node) => drag(drop(node))} style={{ cursor: 'move' }} onMouseEnter={onHover} onMouseLeave={onLeave} className={divClass}>
       <img src={image} className={imgClass} />
       {
         (hover || selected) && <input onChange={onSelect} checked={selected} type="checkbox" className={checkClass} />
