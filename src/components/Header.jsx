7d143040e0a50125e454795d6f417db43232a09a
An7orAhmed <antor.mee@gmail.com>
2023-10-31 16:43:53 +0600

    blur added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,11 @@ import { BiSolidImageAdd } from 'react-icons/bi';
 
 function Header() {
   return (
-    <div className="sticky top-0 navbar bg-base-100 shadow-lg mb-10 py-4">
+    <div className="sticky top-0 navbar bg-base-100 bg-opacity-80 backdrop-blur shadow-lg mb-10 py-4">
       <div className="flex-1">
         <a className="btn btn-ghost normal-case text-xl">Image Gallery</a>
       </div>
       <div className="flex-none gap-3">
-        
         <div className="flex gap-2 items-center btn btn-outline btn-accent">
           <BiSolidImageAdd className="text-xl"></BiSolidImageAdd>Add Image
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 
 function Home() {
   return (
-    <div className="bg-slate-200 pb-10">
+    <div className="bg-slate-100 pb-10">
       <Header></Header>
       <Gallery></Gallery>
     </div>
